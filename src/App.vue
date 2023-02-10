37c42589db74ee28b1372444571c925816ff115d
kbk <bk.kim@theinfoin.com>
2023-02-10 18:38:57 +0900

    리액트보다 100배 쉬운 Vue 반복문 v-for

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+
+  <!-- 반복문 -->
+  <div class="menu">
+    <!-- <v-for="작명" in 횟수" :key="작명">{{ 작명 }} , 키 안쓰면 에러남, 작명은 아무렇게나 가능 -->
+    <a v-for="a in 메뉴들" :key="a">{{ a }}</a> 
+    <!--    <a v-for="(a, i) in 메뉴들" :key="i">{{ a }}</a>    
+            (a,i) a는 array 데이터, i는 0부터 1씩 증가하는 정수  -->
+    <!-- array/object 넣으면 그 자료 안의 데이터 갯수만큼 반복 -->
+  </div>
+
+  <!-- (숙제) 상품 목록도 반복문으로 축약해보셈 -->
+  <!-- 가격은 대충 신경쓰지 맙시다 제목만 잘뜨면 됨 -->
   <div>
-    <!-- {{ 데이터 바인딩 }} : JS 데이터를 HTML에 꽂아넣는 문법 -->
-    원룸샵 <!-- 로고 같은 것은 바뀔 일이 거의 없으므로 데이터 바인딩 ㄴㄴ -->
-    <h4 :style="스타일">XX 원룸</h4> <!-- 태그 안의 데이터 바인딩은 :속성="데이터" -->
-    <P>{{ price1 }} 만원</P>
+    <h4>{{ products[0] }}</h4>
+    <P>40 만원</P>
   </div>
   <div>
-    <h4>XX 원룸</h4>
-    <P>{{ price2 }} 만원</P>
+    <h4>{{ products[1] }}</h4>
+    <P>50 만원</P>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <P>60 만원</P>
   </div>
 </template>
 
-<!-- 숙제 : 원룸 이름만 데이터 바인딩해서 오기 -->
 <script>
-// 데이터 바인딩 이유 1 : html에 하드코딩하면 나중에 변경이 어려움
-// 데이터 바인딩 이유 2 : vue의 실시간 자동 렌더링 쓰기 위함
 export default {
   name: 'App',
-  data(){ // 데이터 보관함
+  data(){ 
     return {
-      // 여기에 모든 데이터를 보관함, object 자료로 저장
-      // 자주 변경될 데이터들은 여기 저장해놨다가 {{}} 사용해서 넣기
-      price1 : 60,
-      price2 : 70,
-      스타일 : 'color : blue',
+      메뉴들 : ['Home', 'Shop', 'About'],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
     }
   },
@@ -40,6 +46,15 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
