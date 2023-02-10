c90562473dd430b67682f3c89f72e4b8ed1b6211
kbk <bk.kim@theinfoin.com>
2023-02-10 19:00:16 +0900

    Vue 이벤트 핸들러로 click 감지하기 (허위매물 신고버튼 만들기)(하는중)

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <template>
 
-  <!-- 반복문 -->
+  <!-- 이벤트 핸들러 -->
   <div class="menu">
-    <!-- <v-for="작명" in 횟수" :key="작명">{{ 작명 }} , 키 안쓰면 에러남, 작명은 아무렇게나 가능 -->
     <a v-for="a in 메뉴들" :key="a">{{ a }}</a> 
-    <!--    <a v-for="(a, i) in 메뉴들" :key="i">{{ a }}</a>    
-            (a,i) a는 array 데이터, i는 0부터 1씩 증가하는 정수  -->
-    <!-- array/object 넣으면 그 자료 안의 데이터 갯수만큼 반복 -->
   </div>
 
-  <!-- (숙제) 상품 목록도 반복문으로 축약해보셈 -->
-  <!-- 가격은 대충 신경쓰지 맙시다 제목만 잘뜨면 됨 -->
-  <div v-for="room in products" :key="room">
-    <h4>{{ room }}</h4>
+  <div>
+    <h4>{{ products[0] }}</h4>
     <P>40 만원</P>
+    <button>허위매물신고</button> <span>신고수 : {{ 신고수 }}</span>
+    <!-- v-on:click == @click -->
   </div>
-
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <P>50 만원</P>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <P>60 만원</P>
+  </div>
 </template>
 
 <script>
@@ -25,6 +28,7 @@ export default {
     return {
       메뉴들 : ['Home', 'Shop', 'About'],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+      신고수 : 0,
     }
   },
   components: {
